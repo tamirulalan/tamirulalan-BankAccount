ea81a43cfdf8d6c5192147c7c7e80f6546dbec1d
Tamiru Taye <tamirulalan@gmail.com>
2023-04-30 09:11:09 +0000

    bank

--- src/main/java/BankService.java
+++ src/main/java/BankService.java
@@ -23,6 +23,7 @@ public class BankService {
      * @param amount the amount to be deposited.
      */
     public void deposit(double amount){
+        balance =balance+amount;
 
     }
 
@@ -32,6 +33,10 @@ public class BankService {
      * @param amount the amount to be withdrawn.
      */
     public void withdraw(double amount){
+        if ( balance >=amount){
+            balance =balance-amount;
+        }
+        
 
     }
 
@@ -40,6 +45,6 @@ public class BankService {
      * @return the user's balance.
      */
     public double getBalance(){
-        return 0;
+        return balance;
     }
 }
